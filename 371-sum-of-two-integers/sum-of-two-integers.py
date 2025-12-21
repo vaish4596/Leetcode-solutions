f722d6db24b873bf488bd8bf21f04f128d9ace12
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2025-12-21 10:33:51 +0530

    Implement alternative sum method using bitwise operations
    Added an alternative method for summing two integers using bitwise operations.

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -2,3 +2,13 @@ class Solution(object):
     def getSum(self, a, b):
         list=[a,b]
         return sum(list)
+
+#or
+class Solution(object):
+    def getSum(self, a, b):
+        while b != 0:
+            x = a ^ b          # sum without carry
+            y = (a & b) << 1   # carry
+            a = x
+            b = y
+        return a
