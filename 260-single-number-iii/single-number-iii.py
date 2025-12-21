bcc35a3c5d94e2814367258c9335de59c25e02e8
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2025-12-21 09:53:20 +0530

    Implement XOR method for finding single numbers
    Added an alternative method to find single numbers using XOR.

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -5,3 +5,22 @@ class Solution(object):
             if nums.count(i) == 1:
                 res.append(i)
         return res
+
+
+#or
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
+
+#method
+res = 0
+res ^= 4 → 4
+res ^= 1 → 5
+res ^= 2 → 7
+res ^= 1 → 6
+res ^= 2 → 4
