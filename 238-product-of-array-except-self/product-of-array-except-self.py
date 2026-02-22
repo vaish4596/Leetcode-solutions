34873b3cd0d970a24ed0923d883fa2e970c70660
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2026-02-22 11:56:02 +0530

    Time: 15 ms (94.47%) | Memory: 25.4 MB (67.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,13 +3,13 @@ class Solution:
         n = len(nums)
         res = [1] * n
         
-        # Prefix product
+        
         prefix = 1
         for i in range(n):
             res[i] = prefix
             prefix *= nums[i]
         
-        # Suffix product
+       
         suffix = 1
         for i in range(n - 1, -1, -1):
             res[i] *= suffix
