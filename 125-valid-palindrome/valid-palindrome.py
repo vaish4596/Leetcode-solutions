1ac36157dc58c4e9c8005c6ca76c2d9f49afc235
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2026-02-27 11:50:05 +0530

    Time: 246 ms (7.48%) | Memory: 13.1 MB (56.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        
         new = ""
         for i in s:
             if i.isalnum():
