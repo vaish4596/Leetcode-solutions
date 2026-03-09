00562d6338cecec3862543c1ed643c67a0bc5343
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2026-03-09 12:51:02 +0530

    Time: 3882 ms (5.59%) | Memory: 20.8 MB (96.93%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,26 +1,8 @@
-class Solution(object):
-    def singleNumber(self, nums):
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
         res = []
         for i in nums:
             if nums.count(i) == 1:
                 res.append(i)
         return res
-
-
-#or
-
-
-class Solution(object):
-    def singleNumber(self, nums):
-        res = 0
-        for num in nums:
-            res ^= num
-        return res
-
-#method
-res = 0
-res ^= 4 → 4
-res ^= 1 → 5
-res ^= 2 → 7
-res ^= 1 → 6
-res ^= 2 → 4
+        
