48ffb07169d0ebd8de82c27fb6ca7baa7ef0e5c0
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2026-03-17 17:17:58 +0530

    Time: 2 ms (35.25%) | Memory: 12.4 MB (89.56%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,7 +3,7 @@ class Solution(object):
         
         res = ""
         for i in range(len(strs[0])):
-            for s in strs:
+            for s in strs: #here it checks column wise means 1st letter of each words then 2nd letter of each word
                 if i == len(s) or s[i] != strs[0][i]:
                     return res
 
