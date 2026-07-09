2661aed4cd9f882d099afdd73198f62e1a796f14
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2026-07-09 20:39:17 +0530

    Time: 11 ms (4.04%) | Memory: 44 MB (15.48%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,16 +1,28 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        ArrayList<Integer> set = new ArrayList<>();
-       for(int i =0; i < m;i++){
-        set.add(nums1[i]);
-       } 
-        for(int i =0; i < n;i++){
-        set.add(nums2[i]);
-       }
-       Collections.sort(set);
-      
-         for (int i = 0; i < set.size(); i++) {
-            nums1[i] = set.get(i); 
+
+    ArrayList<Integer> list = new ArrayList<>();
+
+    for (int i = 0; i < m; i++)
+        list.add(nums1[i]);
+
+    for (int i = 0; i < n; i++)
+        list.add(nums2[i]);
+
+    // Bubble Sort
+    for (int i = 0; i < list.size() - 1; i++) {
+        for (int j = 0; j < list.size() - i - 1; j++) {
+
+            if (list.get(j) > list.get(j + 1)) {
+
+                int temp = list.get(j);
+                list.set(j, list.get(j + 1));
+                list.set(j + 1, temp);
+            }
+        }
     }
-    }
+
+    for (int i = 0; i < list.size(); i++)
+        nums1[i] = list.get(i);
+}
 }
